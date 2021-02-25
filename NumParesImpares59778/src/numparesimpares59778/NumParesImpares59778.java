d81740ed395d5230b6ed0bb029230197bdfdf498
Elias <Elias@DESKTOP-JVU1U2T>
2021-02-24 20:54:16 -0600

    Tarea 2

---
+++ NumParesImpares59778/src/numparesimpares59778/NumParesImpares59778.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Elias
+ */
+public class NumParesImpares59778 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero = 0;
+        numero = +numero;
+        while(numero <= 100){
+            numero = numero + 1;
+            System.out.println(numero);
+          numero++;          
+        }
+            
+                
+                
+                
+    }
+    
+}
