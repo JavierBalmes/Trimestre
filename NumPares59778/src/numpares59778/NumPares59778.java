1bc7edcbfb56de157477e73b1cf93e427bc40558
Elias <Elias@DESKTOP-JVU1U2T>
2021-02-24 20:43:44 -0600

    Tarea

--- NumPares59778/src/numpares59778/NumPares59778.java
+++ NumPares59778/src/numpares59778/NumPares59778.java
@@ -15,7 +15,16 @@ public class NumPares59778 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero = 0;
+        
+        numero = +numero;
+        while (numero <= 84){
+            if (numero % 2 == 0){
+                System.out.println(numero);
+        }
+            numero++;
+            }
+       
     }
     
 }
